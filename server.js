ed2b153f89b1e8eebef6624e363edf4e72088b66
Moshe Bildner <moshe.bildner@gmail.com>
2014-12-15 11:52:37 -0500

    Adds cli to server

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+var yargs = require('yargs')
+	.usage('Run the server for your angular app')
+	.demand('p')
+	.alias('p', 'port')
+	.describe('p', 'Port to serve on')
+	.argv;
+
 var express = require('express');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var path = require('path');
+
+var SERVE_PORT = yargs.port || 8000;
+
 
 var app = express();
 
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
@@ -14,4 +23,6 @@ app.get('/', function (req, res) {
 	res.redirect('dist/index.html');
 });
 
-app.listen(8000);
+app.listen(SERVE_PORT);
+
+console.log('Server is running on ' + SERVE_PORT.toString());
