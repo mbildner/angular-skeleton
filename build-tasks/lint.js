fe7c5aa4f83f90134bb557475b83146dce7bc29c
Moshe Bildner <moshe.bildner@gmail.com>
2015-01-10 09:51:03 -0500

    moves linting options to .jshintrc so they can be easily shared

--- build-tasks/lint.js
+++ build-tasks/lint.js
@@ -4,12 +4,8 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
-var packageJSON  = require('../package');
-var jshintConfig = packageJSON.jshintConfig;
-jshintConfig.lookup = false;
-
 gulp.task('lint', function() {
   return gulp.src('js/**/*.js')
-    .pipe(jshint(jshintConfig))
+    .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
